8b2064c640f3940e34e3209cb93a1aabca18f00f
Chaimae Souiri <77145854+chaimaesouiri@users.noreply.github.com>
2023-11-03 11:57:04 +0100

    Add files via upload

---
+++ iwriter.com/website.php
@@ -0,0 +1,12 @@
+<?php
+
+include_once "content.php";
+
+ $ihost = $_SERVER["HTTP_HOST"] . "/iwriter.com";
+if ($_SERVER['HTTP_HOST']=='localhost' || $_SERVER['HTTP_HOST'] == '127.0.0.1') {
+    $host = "http://".$ihost;
+} else {
+    $host = "https://".$ihost;
+}
+
+$namehost = 'itWriter';
